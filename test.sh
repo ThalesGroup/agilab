448c04158b23d3fed5a92a03794449e638bf36c5
jpmorard <jean-pierre.morard@thalesgroup.com>
2025-03-23 23:07:00 +0100

    Initial version

--- test.sh
+++ test.sh
@@ -4,29 +4,22 @@ set -e
 home=$(pwd)
 
 # List of components to build
-SUBDIRS=("src/fwk/env" "src/fwk/core" "src/fwk/gui")
+SUBDIRS=("fwk/env" "fwk/core" "fwk/gui")
 
 # Prepare output directory
-rm -fr "$home/../agi-pypi"
-mkdir  "$home/../agi-pypi"
+rm -rf "$home/../agi-pypi"
+mkdir "$home/../agi-pypi"
 
-# Build the main project (if needed) as a wheel and move it
+# Build the main project as a sdist and move it
+rm -rf dist
 uv build --sdist
-mv dist/*.tar.gz "$home/../agi-pypi"
+mv dist/*.gz "$home/../agi-pypi"
 
 # Loop through each subdirectory and build accordingly
-for dir in "${SUBDIRS[@]}"; do
-  pushd "$dir" > /dev/null
+for subdir in "${SUBDIRS[@]}"; do
+  pushd "src/$subdir" > /dev/null
   rm -rf dist  # clean previous builds
-  # Build wheel for env and core
   uv build --wheel
-  # Move the resulting .whl to the agi-pypi directory
   mv dist/*.whl "$home/../agi-pypi"
   popd > /dev/null
 done
-
-pushd "$home/../agi-pypi" > /dev/null
-uv init --bare
-uv add *.whl   # include both wheels and sdist in the environment
-uv run agilab
-popd > /dev/null
