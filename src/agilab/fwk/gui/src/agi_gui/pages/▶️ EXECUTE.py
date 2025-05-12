d6c87a60a3c2fbad0beec6ccc87fc2ab1a9c6679
Rému Chen <pmc2000@gmx.fr>
2025-05-12 15:35:13 +0200

    fix path load DF

--- src/agilab/fwk/gui/src/agi_gui/pages/▶️ EXECUTE.py
+++ src/agilab/fwk/gui/src/agi_gui/pages/▶️ EXECUTE.py
@@ -911,10 +911,10 @@ if __name__ == '__main__':
                     except json.JSONDecodeError as e:
                         print("Error decoding JSON:", e)
 
-            st.session_state["loaded_df"] = cached_load_df(env.dataframes_path)
+            st.session_state["loaded_df"] = cached_load_df(Path().home() / env.dataframes_path)
 
         if st.sidebar.button("Load Data", key="load_data"):
-            st.session_state["loaded_df"] = cached_load_df(env.dataframes_path)
+            st.session_state["loaded_df"] = cached_load_df(Path().home() / env.dataframes_path)
         loaded_df = st.session_state.get("loaded_df")
         if isinstance(loaded_df, pd.DataFrame) and not loaded_df.empty:
             st.dataframe(loaded_df)
