fcf49f38f62cccee2115d7d6fb358c7e506fe6d9
Rémy CHEN <pmc2000@gmx.fr>
2025-06-02 15:51:58 +0200

    fix app text worker env init

--- src/agilab/apps/flight_project/test/test_flight_worker.py
+++ src/agilab/apps/flight_project/test/test_flight_worker.py
@@ -20,7 +20,7 @@ sys.path.insert(0,'/home/pcm/wenv/flight_worker/dist')
 
 # AgiWorker.run flight command
 for i in  range(4):
-    env = AgiEnv(install_type=1, verbose=True)
+    env = AgiEnv(install_type=1,active_app="flight_project",verbose=True)
     AgiWorker.new("flight_project", mode=i, env=env, verbose=3, args=args)
     result = AgiWorker.run(workers={"192.168.20.123":2}, mode=i, args=args)
 
