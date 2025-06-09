e6f41d505e6bf64119a1614e97ae7f470cb8f5dd
jpmorard <jean-pierre.morard@thalesgroup.com>
2025-06-09 11:04:06 +0200

    fix enduser install

--- src/agilab/fwk/env/src/agi_env/agi_env.py
+++ src/agilab/fwk/env/src/agi_env/agi_env.py
@@ -726,7 +726,7 @@ class AgiEnv:
         self.gitignore_file = self.app_abs / ".gitignore"
         dest = self.resource_path
         if self.install_type:
-            shutil.copytree(self.core_root.parent / "gui/src/agi_gui" / self.agi_resources, dest, dirs_exist_ok=True)
+            shutil.copytree(self.agi_root / "fwk/gui/src/agi_gui" / self.agi_resources, dest, dirs_exist_ok=True)
         else:
             shutil.copytree(self.agi_root.parent / "agi_gui" / self.agi_resources, dest, dirs_exist_ok=True)
 
