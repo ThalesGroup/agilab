0afbac73cb7a939c385ec8edd31d7f168f914ff8
jpmorard <jean-pierre.morard@thalesgroup.com>
2025-06-10 08:28:27 +0200

    rename setup.py into setup

--- src/agilab/fwk/core/src/agi_core/managers/agi_runner/agi_runner.py
+++ src/agilab/fwk/core/src/agi_core/managers/agi_runner/agi_runner.py
@@ -869,7 +869,7 @@ class AGI:
         logging.info(f"Copying {src / 'pyproject.toml'} -> {wenv_abs}")
         shutil.copy2(src / "pyproject.toml", wenv_abs)
         logging.info(f"Copying {env.setup_core} -> {wenv_abs}")
-        shutil.copy(env.setup_core, wenv_abs / env.setup_core.stem)
+        shutil.copy2(env.setup_core, wenv_abs)
 
         # Commande pour workers selon si rapids supporté
         if has_rapids_hw:
@@ -1360,7 +1360,8 @@ class AGI:
 
         app_path = env.app_abs
         wenv_abs = env.wenv_abs
-        shutil.copy(env.setup_core, app_path / env.setup_core.stem)
+        shutil.copy2(env.setup_core, app_path)
+        shutil.copy2(env.setup_core, app_path)
 
         cmd = f"uv -q --project {app_path} run python {env.setup_app} bdist_egg --packages \"{packages}\" --install_type {env.install_type} -d {wenv_abs}"
         await AgiEnv.run(cmd, app_path)
