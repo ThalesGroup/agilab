33bdc0e728fb25d869acd4b5781c3f4e1ec6e65d
jpmorard <jean-pierre.morard@thalesgroup.com>
2025-06-10 11:58:05 +0200

    clean client_keys

--- src/agilab/fwk/env/src/agi_env/agi_env.py
+++ src/agilab/fwk/env/src/agi_env/agi_env.py
@@ -1044,7 +1044,7 @@ class AgiEnv:
                     username=self.user,
                     password=self.password,
                     known_hosts=None,
-                    client_keys=["/home/pcm/.ssh/id_ed25519"],
+                    client_keys=None,
                 ),
                 timeout=timeout_sec
             )
