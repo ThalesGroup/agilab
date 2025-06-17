8c1f28603ffa284a2854f2eadb65204f624d3194
jpmorard <jean.pierre.morard@gmail.com>
2025-06-17 14:53:43 +0200

    update pycharm script

--- src/agi/fwk/core/test/_test_kill.py
+++ src/agi/fwk/core/test/_test_kill.py
@@ -2,4 +2,4 @@ import runpy
 from agi_env import AgiEnv
 
 env = AgiEnv(active_app="flight", install_type=1, verbose=1)
-runpy.run_path(env.agi_env_root / "src/agi_env/kill.py")
+runpy.run_path(env.manager_root / "sagi_runner/kill.py")
