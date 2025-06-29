a34623e9199aed62b1e2e4786ad893591988e232
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-06-29 21:41:13 +0200

    upgrade python 3.13.5t

--- src/fwk/apps/flight_project/test/_test_flight_worker.py
+++ src/fwk/apps/flight_project/test/_test_flight_worker.py
@@ -34,8 +34,8 @@ async def main():
         'output_format': "csv"
     }
 
-    sys.path.insert(0,'/home/pcm/PycharmProjects/agilab/src/fwk/apps/flight_project/src')
-    sys.path.insert(0,'/home/pcm/wenv/flight_worker/dist')
+    sys.path.insert(0,'/Users/jpm/PycharmProjects/agilab/src/fwk/apps/flight_project/src')
+    sys.path.insert(0,'/Users/jpm/wenv/flight_worker/dist')
 
     env = AgiEnv(active_app="flight", install_type=1, verbose=True)
     cmd = f"{env.uv} run python build.py build_ext --packages 'base_worker, polars_worker' -b {env.wenv_abs}"
@@ -45,7 +45,7 @@ async def main():
     for i in  range(4):
         env = AgiEnv(install_type=1,active_app="flight_project",verbose=True)
         BaseWorker.new("flight_project", mode=i, env=env, verbose=3, args=args)
-        result = BaseWorker.run(workers={"192.168.20.123":2}, mode=i, args=args)
+        result = BaseWorker.run(workers={"192.168.137.1":2}, mode=i, args=args)
         print(result)
 
 if __name__ == "__main__":
