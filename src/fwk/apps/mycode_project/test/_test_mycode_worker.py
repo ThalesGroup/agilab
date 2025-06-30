f973132cf81db1d334bace001d669f15591d14de
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-06-30 09:48:12 +0200

    upgrade python 3.13.5t

--- src/fwk/apps/mycode_project/test/_test_mycode_worker.py
+++ src/fwk/apps/mycode_project/test/_test_mycode_worker.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 from agi_manager import BaseWorker
 from agi_env import AgiEnv
+import asyncio
 
 
 with open(Path().home() / ".local/share/agilab/.core-path",'r') as f:
@@ -15,21 +16,26 @@ path = str(fwk_path / "core/env/src")
 if path not in sys.path:
     sys.path.insert(0, path)
 
-args = {
-    'param1': 0,
-    'param2': "some text",
-    'param3': 3.14,
-    'param4': True
-}
+async def main():
+    args = {
+        'param1': 0,
+        'param2': "some text",
+        'param3': 3.14,
+        'param4': True
+    }
 
-sys.path.insert(0,'/home/pcm/PycharmProjects/agilab/src/fwk/apps/mycode_project/src')
-sys.path.insert(0,'/home/pcm/wenv/mycode_worker/dist')
+    sys.path.insert(0,'/Users/jpm/PycharmProjects/agilab/src/fwk/apps/mycode_project/src')
+    sys.path.insert(0,'/Users/jpm/wenv/mycode_worker/dist')
 
 
-# BaseWorker.run flight command
-for i in  [0,1,3]: # 2 is working only if you have generate the cython lib before
-    env = AgiEnv(install_type=1,active_app="mycode_project",verbose=True)
-    BaseWorker.new('mycode', mode=i, env=env, verbose=3, args=args)
-    result = BaseWorker.run(mode=i, args=args)
+    # BaseWorker.run flight command
+    for i in  [0,1,3]: # 2 is working only if you have generate the cython lib before
+        env = AgiEnv(install_type=1,active_app="mycode_project",verbose=True)
+        BaseWorker.new('mycode', mode=i, env=env, verbose=3, args=args)
+        result = BaseWorker.run(mode=i, args=args)
 
-print(result)
+    print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
