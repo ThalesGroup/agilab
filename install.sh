c50cb5bfac61aa5376674ca7eb043460fefbee59
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-08-11 13:50:57 +0200

    fix install.sh apps

--- install.sh
+++ install.sh
@@ -242,7 +242,7 @@ install_apps() {
 
     echo -e "${BLUE}Installing Apps...${NC}"
     pushd "$apps_dir" > /dev/null
-    ./install.sh "$apps_dir" "1"
+    ./install.sh -debug "$apps_dir" "1"
     popd  > /dev/null
 }
 
