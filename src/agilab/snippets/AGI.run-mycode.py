5a35fb4b00b7d0b3300c4e07bd9a171ada0bf739
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-09-05 08:22:51 +0200

    some fixes

--- src/agilab/snippets/AGI.run-mycode.py
+++ src/agilab/snippets/AGI.run-mycode.py
@@ -6,12 +6,12 @@ from pathlib import Path
 
 async def main():
     app_env = AgiEnv(active_app=Path('/Users/jpm/agilab/src/agilab/apps/mycode_project'), install_type=1, verbose=True) 
-    res = await AGI.run(app_env, mode=0,
+    res = await AGI.run(app_env, mode=4, 
                         scheduler="127.0.0.1", workers={'127.0.0.1': 2}, 
                         verbose=True, param1=0, param2="some text", param3=3.14, param4=True)
     print(res)
     return res
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
             
