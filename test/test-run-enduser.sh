5cb3f01765fe51814dd2c9e6d7c1d835b5a113dd
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-09-06 09:30:56 +0200

    add tests enduser

---
+++ test/test-run-enduser.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rm -r agi-space
+push agi-space
+mkdir agi-space && cd agi-space
+uv init --bare --no-workspace
+uv add -p 3.13  --index-url "https://test.pypi.org/simple" --extra-index-url "https://pypi.org/simple" \
+  --index-strategy unsafe-best-match --upgrade --force-reinstall agilab agi-core
+uv run agilab --openai-api-key "your-api-key"
+popd >/dev/null
