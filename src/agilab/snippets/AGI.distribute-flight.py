0094ebfd056bd9230b496d7c18db36009df948e8
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-09-09 09:42:01 +0200

    cleanup

--- src/agilab/snippets/AGI.distribute-flight.py
+++ src/agilab/snippets/AGI.distribute-flight.py
@@ -7,9 +7,11 @@ from pathlib import Path
 async def main():
     app_env = AgiEnv(active_app=Path('/Users/jpm/agilab/src/agilab/apps/flight_project'), install_type=1, verbose=True)
     res = await AGI.distribute(app_env,
-                               scheduler=None, workers=None, data_source="file", path="data/flight/dataset", files="csv/*", nfile=2, nskip=0, nread=0, sampling_rate=1.0, datemin="2020-01-01", datemax="2021-01-01", output_format="csv")
+                               scheduler=None, 
+                               workers=None,
+                               data_source="file", path="data/flight/dataset", files="csv/*", nfile=2, nskip=0, nread=0, sampling_rate=1.0, datemin="2020-01-01", datemax="2021-01-01", output_format="csv")
     print(res)
     return res
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.get_event_loop().run_until_complete(main())
