fbe3303da88d56915597b23b4dee0ca025ef8eb8
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-09-16 17:27:01 +0200

    remove ~/  from path

--- src/agilab/snippets/AGI.run-sat_trajectory.py
+++ src/agilab/snippets/AGI.run-sat_trajectory.py
@@ -8,7 +8,7 @@ async def main():
     app_env = AgiEnv(active_app=Path('/Users/jpm/agilab/src/agilab/apps/sat_trajectory_project'), install_type=1, verbose=True) 
     res = await AGI.run(app_env, mode=0, 
                         scheduler=None, workers=None, 
-                        path="~/data/sat", data_out="~/data/sat_trajectory/dataframe", data_dir="~/data/sat_trajectory/dataset", duration_s=86400, step_s=1, number_of_sat=25, input_TLE="TLE", input_antenna="antenna_conf.json", input_sat="sat.json")
+                        path="data/sat", data_out="data/sat_trajectory/dataframe", data_dir="data/sat_trajectory/dataset", duration_s=86400, step_s=1, number_of_sat=25, input_TLE="TLE", input_antenna="antenna_conf.json", input_sat="sat.json")
     print(res)
     return res
 
