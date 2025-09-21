6e2db12434555a04a804fc77ee8d68b80ac37b96
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-09-21 20:52:17 +0200

    update

--- src/agilab/apps/flight_project/test/_test_call_worker.py
+++ src/agilab/apps/flight_project/test/_test_call_worker.py
@@ -3,7 +3,7 @@ import asyncio
 
 async def main():
   BaseWorker._new(active_app="flight_worker", mode=0, verbose=True, args={'data_source': 'file', 'data_uri': 'data/flight/dataset', 'files': 'csv/*', 'nfile': 2, 'nskip': 0, 'nread': 0, 'sampling_rate': 1.0, 'datemin': '2020-01-01', 'datemax': '2021-01-01', 'output_format': 'csv'})
-  res = await BaseWorker.run(workers={'127.0.0.1': 1}, mode=0, args={'data_source': 'file', 'data_uri': 'data/flight/dataset', 'files': 'csv/*', 'nfile': 2, 'nskip': 0, 'nread': 0, 'sampling_rate': 1.0, 'datemin': '2020-01-01', 'datemax': '2021-01-01', 'output_format': 'csv'})
+  res = await BaseWorker._run(workers={'127.0.0.1': 1}, mode=0, args={'data_source': 'file', 'data_uri': 'data/flight/dataset', 'files': 'csv/*', 'nfile': 2, 'nskip': 0, 'nread': 0, 'sampling_rate': 1.0, 'datemin': '2020-01-01', 'datemax': '2021-01-01', 'output_format': 'csv'})
   print(res)
 
 if __name__ == '__main__':
--- src/agilab/apps/flight_project/test/_test_flight_worker.py
+++ src/agilab/apps/flight_project/test/_test_flight_worker.py
@@ -44,7 +44,7 @@ async def main():
         if path not in sys.path:
             sys.path.insert(0, path)
         BaseWorker._new(mode=i, env=env, verbose=3, args=args)
-        result = await BaseWorker.run(mode=i, args=args)
+        result = await BaseWorker._run(mode=i, args=args)
         print(result)
 
 if __name__ == "__main__":
