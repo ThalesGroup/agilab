cc8a48c97a52b6e80742dd24c78778bb60da77d9
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-10-13 19:46:54 +0200

    cleanup

--- src/agilab/examples/flight/AGI_run_flight.py
+++ src/agilab/examples/flight/AGI_run_flight.py
@@ -9,9 +9,9 @@ APP = "flight_project"
 async def main():
     app_env = AgiEnv(apps_dir=APPS_DIR, app=APP, verbose=1)
     res = await AGI.run(app_env, 
-                        mode=None, 
-                        scheduler="127.0.0.1", 
-                        workers={'127.0.0.1': 1}, 
+                        mode=15, 
+                        scheduler="192.168.20.111", 
+                        workers={'192.168.20.130': 1}, 
                         data_source="file", data_uri="data/flight/dataset", files="*", nfile=1, nskip=0, nread=0, sampling_rate=1.0, datemin="2020-01-01", datemax="2021-01-01", output_format="parquet")
     print(res)
     return res
