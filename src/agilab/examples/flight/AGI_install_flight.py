b3fa88c1f12d9d8ba95081ba0f615477748ef9be
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-10-13 19:46:54 +0200

    cleanup

--- src/agilab/examples/flight/AGI_install_flight.py
+++ src/agilab/examples/flight/AGI_install_flight.py
@@ -9,7 +9,7 @@ APP = "flight_project"
 async def main():
     app_env = AgiEnv(apps_dir=APPS_DIR, app=APP, verbose=1)
     res = await AGI.install(app_env, 
-                            modes_enabled=None,
+                            modes_enabled=15,
                             scheduler="192.168.20.111", 
                             workers={'192.168.20.130': 1})
     print(res)
