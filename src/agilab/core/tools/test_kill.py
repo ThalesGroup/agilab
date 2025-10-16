d1353b4fa135426a5df7335929d9626f400365f6
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-10-16 14:19:09 +0200

    update

--- src/agilab/core/tools/test_kill.py
+++ src/agilab/core/tools/test_kill.py
@@ -3,4 +3,4 @@ from agi_env import AgiEnv
 
 apps_dir = AgiEnv.locate_agi_installation(verbose=0) / "apps"
 env = AgiEnv(apps_dir=apps_dir, app="flight_project", verbose=1)
-runpy.run_path(env.cluster_root / "src/cluster/cli.py")
+runpy.run_path(env.agi_cluster / "src/cluster/cli.py")
