2fc145e5b2f33a2b627efbd34c62e7a0d274d5ed
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-10-16 14:19:27 +0200

    update

--- src/agilab/examples/flight/AGI_install_flight.py
+++ src/agilab/examples/flight/AGI_install_flight.py
@@ -10,8 +10,8 @@ async def main():
     app_env = AgiEnv(apps_dir=APPS_DIR, app=APP, verbose=1)
     res = await AGI.install(app_env, 
                             modes_enabled=15,
-                            scheduler="192.168.20.111", 
-                            workers={'192.168.20.130': 1})
+                            scheduler="127.0.0.1", 
+                            workers={'127.0.0.1': 1})
     print(res)
     return res
 
