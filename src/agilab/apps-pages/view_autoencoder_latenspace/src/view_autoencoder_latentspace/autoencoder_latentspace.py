748f67a95a3b0476bbad6db95bb6208e79249880
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-10-17 17:46:28 +0200

    docs: align license headers with BSD 3-Clause

--- src/agilab/apps-pages/view_autoencoder_latenspace/src/view_autoencoder_latentspace/autoencoder_latentspace.py
+++ src/agilab/apps-pages/view_autoencoder_latenspace/src/view_autoencoder_latentspace/autoencoder_latentspace.py
@@ -1,7 +1,7 @@
 # BSD 3-Clause License
 #
 # Copyright (c) 2025, Jean-Pierre Morard, THALES SIX GTS FRANCE SAS
-# All rights reserved.
+# Licensed under the BSD 3-Clause License. See LICENSE for details.
 #
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 #
