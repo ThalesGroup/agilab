1c5373394a5121b54c6ba2fef12c469ad0dede6f
Jean-Pierre MORARD <jean-pierre.morard@thalesgroup.com>
2025-10-27 15:05:44 +0100

    update

---
+++ src/agilab/examples/mycode/AGI_run_mycode.py
@@ -0,0 +1,20 @@
+
+import asyncio
+from agi_cluster.agi_distributor import AGI
+from agi_env import AgiEnv
+
+APPS_DIR = "/Users/example/PycharmProjects/agilab/src/agilab/apps"
+APP = "mycode_project"
+
+async def main():
+    app_env = AgiEnv(apps_dir=APPS_DIR, app=APP, verbose=1)
+    res = await AGI.run(app_env, 
+                        mode=15, 
+                        scheduler="127.0.0.1", 
+                        workers={'127.0.0.1': 2}, 
+                        )
+    print(res)
+    return res
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
