e6d9571076731f8f898bba0538bc851afae5e1de
jpmorard <focus@thalesgroup.com>
2025-11-10 17:26:35 +0100

    update

--- src/agilab/examples/mycode/AGI_install_mycode.py
+++ src/agilab/examples/mycode/AGI_install_mycode.py
@@ -19,10 +19,12 @@ APP = "mycode_project"
 
 async def main():
     app_env = AgiEnv(apps_dir=APPS_DIR, app=APP, verbose=1)
-    res = await AGI.install(app_env, 
-                            modes_enabled=15,
-                            scheduler="127.0.0.1", 
-                            workers={'127.0.0.1': 2})
+    res = await AGI.install(
+        app_env,
+        modes_enabled=13,
+        scheduler="127.0.0.1",
+        workers={"127.0.0.1": 2},
+    )
     print(res)
     return res
 
