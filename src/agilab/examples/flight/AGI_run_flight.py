1a81e2dd1f918d1d733ce216a78d3294cb55fb02
jpmorard <focus@thalesgroup.com>
2025-11-10 17:26:35 +0100

    update

--- src/agilab/examples/flight/AGI_run_flight.py
+++ src/agilab/examples/flight/AGI_run_flight.py
@@ -3,16 +3,28 @@ import asyncio
 from agi_cluster.agi_distributor import AGI
 from agi_env import AgiEnv
 
-APPS_DIR = "/Users/example/PycharmProjects/agilab/src/agilab/apps"
+APPS_DIR = "~/PycharmProjects/agilab/src/agilab/apps"
 APP = "flight_project"
 
 async def main():
     app_env = AgiEnv(apps_dir=APPS_DIR, app=APP, verbose=1)
-    res = await AGI.run(app_env, 
-                        mode=13, 
-                        scheduler="192.168.3.84", 
-                        workers={'192.168.3.84': 1, '192.168.3.86': 1}, 
-                        data_source="file", data_in="flight/dataset", data_out="flight/dataframe", files="*", nfile=1, nskip=0, nread=0, sampling_rate=1.0, datemin="2020-01-01", datemax="2021-01-01", output_format="parquet")
+    res = await AGI.run(
+        app_env,
+        mode=13,
+        scheduler="127.0.0.1",
+        workers={"127.0.0.1": 2},
+        data_source="file",
+        data_in="flight/dataset",
+        data_out="flight/dataframe",
+        files="*",
+        nfile=1,
+        nskip=0,
+        nread=0,
+        sampling_rate=1.0,
+        datemin="2020-01-01",
+        datemax="2021-01-01",
+        output_format="parquet",
+    )
     print(res)
     return res
 
