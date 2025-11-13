6e411e38a33bd95331c9c33e2d776afc1c2ff428
jpmorard <focus@thalesgroup.com>
2025-11-13 15:43:22 +0100

    update

---
+++ src/agilab/examples/sat_trajectory/AGI_get_distrib_sat_trajectory.py
@@ -0,0 +1,17 @@
+
+import asyncio
+from agi_cluster.agi_distributor import AGI
+from agi_env import AgiEnv
+
+APPS_DIR = "/Users/agi/PycharmProjects/agilab/src/agilab/apps"
+APP = "sat_trajectory_project"
+
+async def main():
+    app_env = AgiEnv(apps_dir=APPS_DIR, app=APP, verbose=1)
+    res = await AGI.get_distrib(app_env,
+                               data_in="sat_trajectory/dataset", duration_s=86400, step_s=1, number_of_sat=25, input_TLE="TLE", input_antenna="antenna_conf.json", input_sat="sat.json")
+    print(res)
+    return res
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(main())
