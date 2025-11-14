7d567eda8386b5068144eb61ae4b9c5da7ebe205
jpmorard <focus@thalesgroup.com>
2025-11-14 16:16:11 +0100

    fix clone filtering with .gitignore

--- src/agilab/examples/flight/AGI_command_flight.py
+++ src/agilab/examples/flight/AGI_command_flight.py
@@ -1,8 +1,3 @@
-from scipy.signal import convolve2d as cv2
-import numpy as np
+from scipy.signal import savgol_filter
 
-# Define the window size for Savitzky-Golay filtering
-window_size = 51
-
-# Apply Savitzky-Golay filter to column 'long'
-df['smoothed_long'] = cv2(df['long'].values, window_size, 2) / window_size
+df['smoothed_long'] = savgol_filter(df['long'], window_length=5, polyorder=2)
