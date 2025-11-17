c4100e12512b5f5a465718e8980f6dd1dd666a3c
jpmorard <focus@thalesgroup.com>
2025-11-17 17:34:23 +0100

    update

--- src/agilab/examples/sat_trajectory/AGI_run_sat_trajectory.py
+++ src/agilab/examples/sat_trajectory/AGI_run_sat_trajectory.py
@@ -9,8 +9,10 @@ APP = "sat_trajectory_project"
 async def main():
     app_env = AgiEnv(apps_dir=APPS_DIR, app=APP, verbose=1)
     res = await AGI.run(app_env, 
-                        mode=0, 
-                        data_in="sat_trajectory/dataset", duration_s=86400, step_s=1, number_of_sat=25, input_TLE="TLE", input_antenna="antenna_conf.json", input_sat="sat.json")
+                        mode=15, 
+                        scheduler="192.168.20.111", 
+                        workers={'127.0.0.1': 1}, 
+                        data_in="/Users/agi/sat_trajectory/dataset", duration_s=86400, step_s=1, number_of_sat=25, input_TLE="TLE", input_antenna="antenna_conf.json", input_sat="sat.json")
     print(res)
     return res
 
