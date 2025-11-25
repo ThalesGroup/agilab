ef1bed969135f2e99839d5e8f911b86aaba24457
jpmorard <focus@thalesgroup.com>
2025-11-25 09:59:47 +0100

    update

---
+++ src/agilab/apps/sb3_trainer_worker/src/sb3_trainer_worker/__init__.py
@@ -0,0 +1,7 @@
+from .sb3_trainer_worker import *
+from .envs.time_routing_env import TimeRoutingEnv
+
+__all__ = [
+    "Sb3TrainerWorker",
+    "TimeRoutingEnv",
+]
---
+++ src/agilab/apps/sb3_trainer_worker/src/sb3_trainer_worker/envs/__init__.py
@@ -0,0 +1,5 @@
+"""Environment package for SB3 trainer worker."""
+
+from .time_routing_env import TimeRoutingEnv
+
+__all__ = ["TimeRoutingEnv"]
