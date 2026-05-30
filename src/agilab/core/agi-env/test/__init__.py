3e93ac6d82025235498e719d4968a27566be385b
Jean-Pierre MORARD <203319130+jpmorard@users.noreply.github.com>
2026-05-30 11:08:04 +0200

    test: avoid duplicate conftest package imports

--- src/agilab/core/agi-env/test/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Namespace package shim so AgiEnv tests share the global ``test`` package."""
-
-from pkgutil import extend_path
-
-__path__ = extend_path(__path__, __name__)  # type: ignore[misc]
